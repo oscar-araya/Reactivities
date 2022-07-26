df27ac531d0ace37fa103b9c1a23624932559175
Oscar Araya <ocleac@gmail.com>
2022-07-26 12:45:41 -0600

    End of section 5

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,13 +18,13 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id = id});
+            return await Mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command { Activity = activity}));
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")]
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,94 @@
+import { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+	activity: Activity | undefined;
+	closeForm: () => void;
+	createOrEdit: (activity: Activity) => void;
+}
+const ActivityForm = ({
+	activity: selectedActivity,
+	closeForm,
+	createOrEdit,
+}: Props) => {
+	const initialState = selectedActivity ?? {
+		id: "",
+		title: "",
+		date: "",
+		description: "",
+		category: "",
+		city: "",
+		venue: "",
+	};
+
+	const [activity, setActivity] = useState(initialState);
+
+	const handleSubmit = () => {
+		createOrEdit(activity)
+	};
+
+	function handleInputChage(
+		event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+	) {
+		const { name, value } = event.target;
+		setActivity({ ...activity, [name]: value });
+	}
+
+	return (
+		<Segment clearing>
+			<Form onSubmit={handleSubmit} autoComplete="off">
+				<Form.Input
+					placeholder="Title"
+					value={activity.title}
+					name="title"
+					onChange={handleInputChage}
+				/>
+				<Form.TextArea
+					placeholder="Description"
+					value={activity.description}
+					name="description"
+					onChange={handleInputChage}
+				/>
+				<Form.Input
+					placeholder="Category"
+					value={activity.category}
+					name="category"
+					onChange={handleInputChage}
+				/>
+				<Form.Input
+					placeholder="Date"
+					value={activity.date}
+					name="date"
+					onChange={handleInputChage}
+				/>
+				<Form.Input
+					placeholder="City"
+					value={activity.city}
+					name="city"
+					onChange={handleInputChage}
+				/>
+				<Form.Input
+					placeholder="Venue"
+					value={activity.venue}
+					name="venue"
+					onChange={handleInputChage}
+				/>
+				<Button
+					floated="right"
+					positive
+					type="submit"
+					content="Submit"
+				/>
+				<Button
+					onClick={closeForm}
+					floated="right"
+					type="button"
+					content="Cancel"
+				/>
+			</Form>
+		</Segment>
+	);
+};
+
+export default ActivityForm
